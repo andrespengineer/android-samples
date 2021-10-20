1598df8f3cfb8ea5ebb22587197022b411abd46c
Andrés Sierra Pena <andrespengineer@gmail.com>
2021-10-20 03:13:56 -0400

    Adding interface for BaseFragment and BaseActivity

---
+++ app/src/main/java/com/social/presentation/common/CommonActions.kt
@@ -0,0 +1,8 @@
+package com.social.presentation.common
+
+interface CommonActions {
+    fun requestPermissions() { /* Default impl */ }
+    fun showLoading(show: Boolean) { /* Default impl */ }
+    fun collectViewModels() { /* Default impl */ }
+    fun fetchData() { /* Default Impl */ }
+}
